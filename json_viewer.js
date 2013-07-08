099eb381083ed7f70505c707dd55a3ffda833a27
Hiro <mh19820223@gmail.com>
2013-07-09 08:28:09 +0900

    add id

--- json_viewer.js
+++ json_viewer.js
@@ -1,12 +1,13 @@
 var json_viewer = {};
-var id = 0
-var indent_width = 20
+var indent_width = 20;
+json_viewer._id = 0;
 json_viewer._data = {"name": "foo", "list":[1,{fff: 1, er:"ty"},2,3,4,5], "obj":{fff: 1, er:"ty"}};
 json_viewer._put = function(data, indent, key) {
     key = key === undefined ? "": key;
     var left = indent_width * indent;
     if (data.toString() === "[object Object]") {
-        document.body.innerHTML += "<div style=\"left:" + left + "px;\">" + key + ":{}</div>"
+        this._id += 1; 
+        document.body.innerHTML += "<div id=\"" + this._id + "\" style=\"left:" + left + "px;\">" + key + ":{}</div>"
         var put = json_viewer._put.bind(this)
         indent += 1
         Object.keys(data).map(function(key) {
@@ -14,7 +15,8 @@ json_viewer._put = function(data, indent, key) {
         })
     }
     else if (Array.isArray(data)) {
-        document.body.innerHTML += "<div style=\"left:" + left + "px;<div\">" + key + ":[]</div>"
+        this._id += 1; 
+        document.body.innerHTML += "<div id=\"" + this._id + "\"  style=\"left:" + left + "px;<div\">" + key + ":[]</div>"
         var put = json_viewer._put.bind(this)
         indent += 1
         data.map(function(value, index) {
@@ -22,16 +24,20 @@ json_viewer._put = function(data, indent, key) {
         })
     }
     else if (data.constructor === String) {
-        document.body.innerHTML += "<div style=\"left:" + left + "px;\">" + key + ":\"" + data + "\"</div>"
+        this._id += 1; 
+        document.body.innerHTML += "<div id=\"" + this._id + "\" style=\"left:" + left + "px;\">" + key + ":\"" + data + "\"</div>"
     }
     else if (data === null) {
-        document.body.innerHTML += "<div style=\"left:" + left + "px;\">" + key + ":null</div>"
+        this._id += 1; 
+        document.body.innerHTML += "<div id=\"" + this._id + "\"  style=\"left:" + left + "px;\">" + key + ":null</div>"
     }
     else if (data === undefined) {
-        document.body.innerHTML += "<div style=\"left:" + left + "px;\">" + key + ":undefined</div>"
+        this._id += 1; 
+        document.body.innerHTML += "<div id=\"" + this._id + "\"  style=\"left:" + left + "px;\">" + key + ":undefined</div>"
     }
     else {
-        document.body.innerHTML += "<div style=\"left:" + left + "px;\">" + key + ":" + data + "</div>"
+        this._id += 1; 
+        document.body.innerHTML += "<div id=\"" + this._id + "\"  style=\"left:" + left + "px;\">" + key + ":" + data + "</div>"
     }
 }
 
@@ -39,3 +45,4 @@ json_viewer.display = function() {
     this._put(this._data, 0);
 }
 
+json_viewer.display()
