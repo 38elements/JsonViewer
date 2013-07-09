2152f55846957cca2d2282aa899ae3179d140476
Hiro <mh19820223@gmail.com>
2013-07-09 18:19:48 +0900

    add _put_*

--- json_viewer.js
+++ json_viewer.js
@@ -4,11 +4,7 @@ json_viewer._id = 0;
 json_viewer.root = "root";
 json_viewer._data = {"name": "foo", "list":[1,{fff: 1, er:"ty"},2,3,4,5], "obj":{fff: 1, er:"ty"}};
 
-json_viewer._put = function(data, indent, key, parent_id) {
-    key = key === undefined ? "": key;
-    parent_id = parent_id === undefined ? this.root: parent_id;
-    var left = this._indent_width * indent;
-    if (data.toString() === "[object Object]") {
+json_viewer._put_object = function(data, parent_id, left, key, indent) {
         this._id += 1; 
         document.body.innerHTML += "<div data-state=\"1\" parent_id=\"" + parent_id + "\" id=\"" + this._id + "\" style=\"left:" + left + "px;\" data_type=\"Object\" ><div class=\"mark\">+</div><span class=\"key_value\">" + key + ":{}</span></div>"
         indent += 1
@@ -16,8 +12,9 @@ json_viewer._put = function(data, indent, key, parent_id) {
         Object.keys(data).map(function(key) {
             this._put(data[key], indent, key, next_parent_id) 
         }, this)
-    }
-    else if (Array.isArray(data)) {
+}
+
+json_viewer._put_array = function(data, parent_id, left, key, indent) {
         this._id += 1; 
         document.body.innerHTML += "<div data-state=\"1\"  parent_id=\"" + parent_id + "\" id=\"" + this._id + "\"  style=\"left:" + left + "px;\" data_type=\"Array\" ><div class=\"mark\">+</div><span class=\"key_value\">" + key + ":&nbsp;[]</span></div>"
         indent += 1
@@ -25,22 +22,49 @@ json_viewer._put = function(data, indent, key, parent_id) {
         data.map(function(value, index) {
             this._put(value, indent, index, next_parent_id) 
         }, this)
-    }
-    else if (data.constructor === String) {
+}
+
+json_viewer._put_string = function(data, parent_id, left, key) {
         this._id += 1; 
         document.body.innerHTML += "<div data-state=\"1\"  parent_id=\"" + parent_id + "\" id=\"" + this._id + "\" style=\"left:" + left + "px;\" data_type=\"String\" ><span class=\"key_value\">" + key + ":&nbsp;\"" + data + "\"</span></div>"
-    }
-    else if (data === null) {
+}
+
+json_viewer._put_null = function(data, parent_id, left, key) {
         this._id += 1; 
         document.body.innerHTML += "<div data-state=\"1\"  parent_id=\"" + parent_id + "\" id=\"" + this._id + "\"  style=\"left:" + left + "px;\" data_type=\"null\" ><span class=\"key_value>\"" + key + ":&nbsp;null</span></div>"
-    }
-    else if (data === undefined) {
+}
+
+json_viewer._put_undefined = function(data, parent_id, left, key) {
         this._id += 1; 
         document.body.innerHTML += "<div data-state=\"1\"  parent_id=\"" + parent_id + "\" id=\"" + this._id + "\"  style=\"left:" + left + "px;\" data_type=\"undefined\" ><span class=\"key_value\">" + key + ":&nbsp;undefined</span></div>"
-    }
-    else {
+}
+
+json_viewer._put_number = function(data, parent_id, left, key) {
         this._id += 1; 
         document.body.innerHTML += "<div data-state=\"1\"  parent_id=\"" + parent_id + "\" id=\"" + this._id + "\"  style=\"left:" + left + "px;\" data_type=\"Number\" ><span class=\"key_value\">" + key + ":&nbsp;" + data + "</span></div>"
+}
+
+json_viewer._put = function(data, indent, key, parent_id) {
+    key = key === undefined ? "": key;
+    parent_id = parent_id === undefined ? this.root: parent_id;
+    var left = this._indent_width * indent;
+    if (data.toString() === "[object Object]") {
+        this._put_object(data, parent_id, left, key, indent);
+    }
+    else if (Array.isArray(data)) {
+        this._put_array(data, parent_id, left, key, indent);
+    }
+    else if (data.constructor === String) {
+        this._put_string(data, parent_id, left, key);
+    }
+    else if (data === null) {
+        this._put_null(data, parent_id, left, key);
+    }
+    else if (data === undefined) {
+        this._put_undefined(data, parent_id, left, key);
+    }
+    else {
+        this._put_number(data, parent_id, left, key);
     }
 }
 
