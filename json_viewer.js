8ae03e1800adf9faed9aaaed152e2e45756a7163
Hiro <mh19820223@gmail.com>
2013-07-09 11:16:15 +0900

    add close

--- json_viewer.js
+++ json_viewer.js
@@ -1,11 +1,13 @@
 var json_viewer = {};
-var indent_width = 20;
+json_viewer._indent_width = 20;
 json_viewer._id = 0;
+json_viewer.root = "root";
 json_viewer._data = {"name": "foo", "list":[1,{fff: 1, er:"ty"},2,3,4,5], "obj":{fff: 1, er:"ty"}};
+
 json_viewer._put = function(data, indent, key, parent_id) {
     key = key === undefined ? "": key;
-    parent_id = parent_id === undefined ? "": parent_id;
-    var left = indent_width * indent;
+    parent_id = parent_id === undefined ? this.root: parent_id;
+    var left = this._indent_width * indent;
     if (data.toString() === "[object Object]") {
         this._id += 1; 
         document.body.innerHTML += "<div parent_id=\"" + parent_id + "\" id=\"" + this._id + "\" style=\"left:" + left + "px;\">" + key + ":{}</div>"
@@ -40,6 +42,22 @@ json_viewer._put = function(data, indent, key, parent_id) {
         this._id += 1; 
         document.body.innerHTML += "<div parent_id=\"" + parent_id + "\" id=\"" + this._id + "\"  style=\"left:" + left + "px;\">" + key + ":" + data + "</div>"
     }
+}
+
+json_viewer._close = function() {
+    var target_parent_id = this.id;
+    var child_list = document.querySelectorAll("[parent_id=\"" + target_parent_id + "\"]");
+    var children = Array.prototype.slice.call(child_list);
+    children.map(function(c){
+        console.log(c.id)
+        c.style.display = "none";
+        json_viewer._close.call(c);
+    });
+}
+
+json_viewer.close_all = function() {
+    var root =  document.querySelector("[parent_id=\"" + this.root + "\"]");
+    this._close.call(root);
 }
 
 json_viewer.display = function() {
